9990d0f541486c3eae069c5f13a98f8bcf45b38f
Rani Saed <ranisaed@Ranis-MacBook-Air.local>
2025-06-08 22:39:13 +0300

    🚀 Initial full push of project to new repo

---
+++ start-dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "🚀 Starting port-forwarding for all services..."
+
+# Main services
+kubectl port-forward service/backend 5000:5000 &
+kubectl port-forward service/frontend 3000:80 &
+kubectl port-forward service/pgadmin 5050:80 &
+
+# Monitoring (Prometheus and Grafana)
+kubectl port-forward -n prometheus service/prometheus 9090:9090 &
+kubectl port-forward -n prometheus service/grafana 3001:3000 &
+
+echo "✅ All services are being forwarded!"
